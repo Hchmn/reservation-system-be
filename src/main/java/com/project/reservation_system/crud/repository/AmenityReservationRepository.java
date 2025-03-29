c111c5ed2106df0c9137f1ac5e3f2d68550239b4
Nas <nas@solutionsresource.com>
2025-03-29 22:46:13 +0800

    [EDIT] - modified the amenity reservation repo.

--- src/main/java/com/project/reservation_system/crud/repository/AmenityReservationRepository.java
+++ src/main/java/com/project/reservation_system/crud/repository/AmenityReservationRepository.java
@@ -3,6 +3,8 @@ package com.project.reservation_system.crud.repository;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -17,4 +19,15 @@ public interface AmenityReservationRepository extends JpaRepository<AmenityReser
             "(ar.startDateTime <= ?2 AND ar.endDateTime >= ?3) )")
     List<AmenityReservation> checkAmenityReservation(Amenities amenity, LocalDateTime startDateTime,
             LocalDateTime endDateTime);
+
+@Query("SELECT ar FROM AmenityReservation ar WHERE " +
+            "(CASE WHEN ?1 = '' THEN 1 ELSE 0 END = 1) OR " +
+            "(LOWER(ar.status) LIKE LOWER(CONCAT('%', ?1, '%')) OR " +
+            "LOWER(ar.remarks) LIKE LOWER(CONCAT('%', ?1, '%')) OR " +
+            "LOWER(ar.purpose) LIKE LOWER(CONCAT('%', ?1, '%')) OR " +
+            "LOWER(ar.client.firstName) LIKE LOWER(CONCAT('%', ?1, '%')) OR " +
+            "LOWER(ar.client.lastName) LIKE LOWER(CONCAT('%', ?1, '%')) OR " +
+            "LOWER(ar.user.username) LIKE LOWER(CONCAT('%', ?1, '%')) OR " +
+            "LOWER(ar.amenities.name) LIKE LOWER(CONCAT('%', ?1, '%')))")  
+    Page<AmenityReservation> searchAmenityReservationsByKeyword(String keyword, Pageable pageable);
 }
